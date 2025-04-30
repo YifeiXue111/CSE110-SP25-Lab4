ac0b17351a9069ffc8bebd6b71ecefbe4ba89e6d
YifeiXue111 <156253841+YifeiXue111@users.noreply.github.com>
2025-04-30 00:15:05 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,10 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return false;
+    }
+    if (add === true) {
         let result = 0;
 
         result = num1 + num2;
@@ -27,11 +30,19 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    if (length === 0) {
+    let discountedPrice = 0
+
+    if (length === 0 || Array.isArray(prices) === false) {
         return false;
     }
-    let discountedPrice = 0
+    if (typeof discount !== 'number' || discount < 0 || discount > 1) {
+        return false;
+      }
+
     for(let i = 0; i < length; i++) {
+        if (typeof prices[i] !== 'number') {
+            return false;
+        }
         discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
@@ -40,3 +51,6 @@ function discountPrices(prices, discount) {
 }
 
 module.exports = {sumValues, discountPrices};
+
+console.log(sumValues('not a number', 2, false));
+console.log(discountPrices('not an arary', 0.2));
